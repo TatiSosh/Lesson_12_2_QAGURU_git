a3251b2ed98a2b3d88e3777d3b1c6c3c8f3f3c65
TatiSosh <grunya1986@gmail.com>
2022-04-16 12:49:37 +0300

    add new banch

--- src/com/github/TatiSosh/Main.java
+++ src/com/github/TatiSosh/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Проверка связи)");
-        System.out.println("А то что-то не работало.");
+        System.out.println("А то что-то не работало");
     }
 }
