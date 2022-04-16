f30e6cb988dc447393978de9036f00a8fe25e632
TatiSosh <grunya1986@gmail.com>
2022-04-16 12:20:30 +0300

    change text

--- src/com/github/TatiSosh/Main.java
+++ src/com/github/TatiSosh/Main.java
@@ -3,6 +3,6 @@ package com.github.TatiSosh;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Save the Queen!");
+        System.out.println("Проверка связи)");
     }
 }
